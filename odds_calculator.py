14f61b601cc25a3f3fff62a8656d0023a2f81b8f
MattMDeLuca <mattmdeluca@yahoo.com>
2016-07-12 18:32:18 -0400

    New Changes
    Updated to show the order of the picks.

--- odds_calculator.py
+++ odds_calculator.py
@@ -4,6 +4,9 @@ import json
 with open('combos.json') as f:
     team_combos = json.load(f)
 
+order = []
+count = 0
+
 while True:
     print
     pick = raw_input("Enter team chosen: ")
@@ -22,3 +25,7 @@ while True:
         combos_1 = len(v)
         percent = (float(combos_1) / total_combos) * 100
         print k.ljust(7), str(combos_1).ljust(3), "{:.4}%".format(str(percent))
+    count +=1
+    order.append(count)
+    order.append(pick)
+    print order
